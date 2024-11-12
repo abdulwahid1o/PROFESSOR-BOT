61032c8fad8f8cd46adcdbbf0141d173d9201030
abdulwahid1o <110869975+abdulwahid1o@users.noreply.github.com>
2024-11-13 01:45:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,8 +16,8 @@ API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
-WEB_SUPPORT = bool(environ.get("WEBHOOK", 'True')) # for web support on/off
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+WEB_SUPPORT = bool(environ.get("WEBHOOK", 'False')) # for web support on/off
+PICS = (environ.get('PICS' ,'https://graph.org/file/2ab1531a294ab79eb15ba.jpg https://graph.org/file/3586f33119174c2d22b37.jpg https://graph.org/file/69e0678bce4630a4d0298.jpg' )).split()
 UPTIME = time.time()
 
 # Admins, Channels & Users
@@ -73,9 +73,4 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled(environ.get('MELCOW_NEW_USERS', "True"), True)
 PROTECT_CONTENT = is_enabled(environ.get('PROTECT_CONTENT', "False"), False)
 PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"), True)
-LOG_MSG = "{} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n🖥️ Rᴇᴏᴩ: {}\n🉐 Vᴇʀsɪᴏɴ: {}\n🧾 Lɪᴄᴇɴꜱᴇ: {}\n©️ Cᴏᴩʏʀɪɢʜᴛ: {}"
-
-
-
-
-
+LOG_MSG = "{} Iêœ± Rá´‡sá´›á´€Ê€á´›á´‡á´…....âœ¨\n\nðŸ—“ï¸ Dá´€á´›á´‡ : {}\nâ° TÉªá´á´‡ : {}\n\nðŸ–¥ï¸ Rá´‡á´á´©: {}\nðŸ‰ Vá´‡Ê€sÉªá´É´: {}\nðŸ§¾ LÉªá´„á´‡É´êœ±á´‡: {}\nÂ©ï¸ Cá´á´©ÊÊ€ÉªÉ¢Êœá´›: {}"
